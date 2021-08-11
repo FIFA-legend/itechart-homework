be33c5ad9f8b64c308e79935c26646316c95916b
FIFA_legend <kolodkonikitos@gmail.com>
2021-08-11 18:30:42 +0300

    Added methods for fibonacci numbers and atkin sieve

--- src/main/scala/com/itechart/basics/Basics.scala
+++ src/main/scala/com/itechart/basics/Basics.scala
@@ -6,7 +6,7 @@ object Basics {
 
   def main(args: Array[String]): Unit = {
     println(fibonacci(10))
-    println(atkinSieve())
+    println(atkinSieve(97))
   }
 
   def fibonacci(n: Int): Either[String, Int] = {
@@ -22,30 +22,40 @@ object Basics {
     else Right(go(0, 1, n))
   }
 
-  def atkinSieve(n: Int = 10000): Unit = {
-    val array = Array.fill(n + 1)(false)
-    array(2) = true
-    array(3) = true
-    val sqrt = Math.sqrt(n).toInt
-    for {
-      x <- 1 until sqrt
-      y <- 1 until sqrt
-      z = (4 * x * x) + (y * y)
-      if z <= n && (z % 12 == 1 || z % 12 == 5)
-    } yield array(z) = !array(z)
-    for {
-      x <- 1 until sqrt
-      y <- 1 until sqrt
-      z = (3 * x * x) + (y * y)
-      if z <= n && (z % 12 == 7)
-    } yield array(z) = !array(z)
-    for {
-      x <- 1 until sqrt
-      y <- 1 until sqrt
-      z = (3 * x * x) - (y * y)
-      if x > y && z <= n && (z % 12 == 11)
-    } yield array(z) = !array(z)
-    println(array.toList)
+  def atkinSieve(n: Int = 10000): Either[String, List[Int]] = {
+    if (n <= 1) Left("n should be greater than 1")
+    else if (n == 2) Right(List(2))
+    else {
+      val array = Array.fill(n + 1)(false)
+      array(2) = true
+      array(3) = true
+      val sqrt = Math.sqrt(n).toInt
+      for {
+        x <- 1 to sqrt
+        y <- 1 to sqrt
+        z = (4 * x * x) + (y * y)
+        if z <= n && (z % 12 == 1 || z % 12 == 5)
+      } yield array(z) = !array(z)
+      for {
+        x <- 1 to sqrt
+        y <- 1 to sqrt
+        z = (3 * x * x) + (y * y)
+        if z <= n && (z % 12 == 7)
+      } yield array(z) = !array(z)
+      for {
+        x <- 1 to sqrt
+        y <- 1 to sqrt
+        z = (3 * x * x) - (y * y)
+        if x > y && z <= n && (z % 12 == 11)
+      } yield array(z) = !array(z)
+      for {
+        a <- 5 to sqrt
+        if array(a)
+        x = a * a
+        i <- x to n by x
+      } yield array(i) = false
+      Right(array.zipWithIndex.filter(pair => pair._1).map(pair => pair._2).toList)
+    }
   }
 
 }

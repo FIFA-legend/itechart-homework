1f6a921f5f0e4140a596843630fb7e07b6f1a21c
FIFA_legend <kolodkonikitos@gmail.com>
2021-08-12 13:29:35 +0300

    A new version of Atkin sieve is written in more functional way

--- src/main/scala/com/itechart/basics/Basics.scala
+++ src/main/scala/com/itechart/basics/Basics.scala
@@ -6,7 +6,8 @@ object Basics {
 
   def main(args: Array[String]): Unit = {
     println(fibonacci(10))
-    println(atkinSieve(97))
+    println(atkinSieve(17))
+    println(atkinSieve2(97))
   }
 
   def fibonacci(n: Int): Either[String, Int] = {
@@ -55,6 +56,41 @@ object Basics {
         i <- x to n by x
       } yield array(i) = false
       Right(array.zipWithIndex.filter(pair => pair._1).map(pair => pair._2).toList)
+    }
+  }
+
+  def atkinSieve2(n: Int = 10000): Either[String, List[Int]] = {
+    def sieve(list: List[Boolean], f: (Int, Int) => Int, condition: (Int, Int, Int, Int) => Boolean): List[Boolean] = {
+      val sqrt = Math.sqrt(n).toInt
+      val allValues = for {
+        x <- 1 to sqrt
+        y <- 1 to sqrt
+        z = f(x, y)
+        if condition(x, y, z, n)
+      } yield z
+      list.zipWithIndex
+        .map { case (bool, index) => if (allValues.contains(index)) !bool else bool }
+    }
+
+    if (n <= 1) Left("n should be greater than 1")
+    else if (n == 2) Right(List(2))
+    else {
+      val list = false :: false :: true :: true :: List.fill(n - 3)(false)
+      val sqrt = Math.sqrt(n).toInt
+      val list1 = sieve(list, (x, y) => (4 * x * x) + (y * y), (_, _, z, n) => z <= n && (z % 12 == 1 || z % 12 == 5))
+      val list2 = sieve(list1, (x, y) => (3 * x * x) + (y * y), (_, _, z, n) => z <= n && (z % 12 == 7))
+      val list3 = sieve(list2, (x, y) => (3 * x * x) - (y * y), (x, y, z, n) => x > y && z <= n && (z % 12 == 11))
+      val allValues = for {
+        a <- 5 to sqrt
+        if list3(a)
+        x = a * a
+        i <- x to n by x
+      } yield i
+      Right(
+        list3.zipWithIndex
+          .filter { case (bool, index) => if (allValues.contains(index)) false else bool}
+          .map { case (_, index) => index }
+      )
     }
   }
 
